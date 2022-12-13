ab5bffdbb2c324b860957fb1a267a319ab9681a2
koredeycode <koredey4u@gmail.com>
2022-12-13 07:57:38 +0000

    solved a bug in 13th task

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (tree->left != NULL || tree->right == NULL)
+		if (tree->left != NULL || tree->right != NULL)
 			count += 1;
 		else
 			count += 0;
